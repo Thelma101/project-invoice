4cce55381feed0a091b19cee88fcbe2efd8b4cfd
DCTP-BE <akpata.thelma@gmail.com>
2024-03-14 13:01:36 +0100

    v1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+import avatar from '../assets/image-avatar.jpg';
+import sun from '../assets/icon-sun.svg';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='flex flex-col h-auto mx-auto rounded w-18 bg-slate-600'>
+        <div>
+          <img src={logo} />
+        
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
