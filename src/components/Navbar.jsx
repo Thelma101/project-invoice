dabda31d95407decabf5fee5e70f0841a68a40ad
DCTP-BE <akpata.thelma@gmail.com>
2024-03-14 16:47:57 +0100

    v2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,21 +6,22 @@ import sun from '../assets/icon-sun.svg';
 const Navbar = () => {
   return (
     <div className=''>
-      <div className='fixed flex flex-col h-[100%] mx-auto rounded-e-3xl w-18 bg-slate-600'>
+      <div className='fixed flex flex-col h-[100%] mx-auto rounded-e-3xl w-16 bg-slate-600'>
         <div>
           <img src={logo} />
-      </div>
-      <div className='gap-8'>
-        <div className>
-          <img src={sun} />
         </div>
-        <div>
-          <img src={avatar} />
+        <div className='absolute flex flex-col items-center justify-center gap-4 bottom-3'>
+          <div className='items'>
+            <img src={sun} />
+          </div>
+          <div className='relative block w-24 h-[1.5px] bg-slate-400'></div>
+          <div>
+            <img src={avatar} className='w-12 h-12 rounded-full' />
+          </div>
         </div>
       </div>
-    </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
