d3b2240eda9bbdcaeb296e14f2f068d48c3ba7af
DCTP-BE <akpata.thelma@gmail.com>
2024-03-18 16:16:53 +0100

    v3

--- src/components/NewInvoice.jsx
+++ src/components/NewInvoice.jsx
@@ -41,26 +41,26 @@ const NewInvoice = () => {
                     </div>
                     <div className='flex flex-col'>
                         <label className=''>PostCode</label>
-                        <input type='text' name='postCode' id={postCode} value={formInputData.postCode} onChange={handleChange} placeholder='102105' className='p-3 border-2 border-gray-700 rounded-md' />
+                        <input type='text' name='postCode'  placeholder='102105' id='postCode' value={formInputData.postCode} onChange={handleChange} className='p-3 border-2 border-gray-700 rounded-md' />
                     </div>
                     <div className='flex flex-col pl-2'>
                         <label className=''>Country</label>
-                        <input type='text' name='country' placeholder='Nigeria' id={country} value={formInputData.country} onChange={handleChange} className='p-3 border-2 border-gray-700 rounded-md' />
+                        <input type='text' name='country' placeholder='Nigeria' id='country' value={formInputData.country} onChange={handleChange} className='p-3 border-2 border-gray-700 rounded-md' />
                     </div>
                 </div>
 
                 <div className='flex flex-col mt-8' >
                     <p className='text-[#7c5dfa]'>Bill To</p>
                     <label>Client Name</label>
-                    <input type='text' name='clientName' id={clientName} value={formInputData.clientName} placeholder='Tee' className='p-3 border-2 border-gray-700 rounded-md' />
+                    <input type='text' name='clientName' id='clientName' value={formInputData.clientName} onChange={handleChange} placeholder='Tee' className='p-3 border-2 border-gray-700 rounded-md' />
                 </div>
                 <div className='flex flex-col mt-8'>
                     <label className=''>Client Email</label>
-                    <input type='text' name='email' id={email} value={formInputData.email} placeholder='tee@mail.com' className='p-3 border-2 border-gray-700 rounded-md' />
+                    <input type='text' name='email' id='email' value={formInputData.email} onChange={handleChange} placeholder='tee@mail.com' className='p-3 border-2 border-gray-700 rounded-md' />
                 </div>
                 <div className='flex flex-col mt-8'>
                     <label className=''>Client Address</label>
-                    <input type='text' name='clientAddress' id={clientAddress} value={formInputData.clientAddress} placeholder='14x Lekki' className='p-3 border-2 border-gray-700 rounded-md' />
+                    <input type='text' name='clientAddress' id='clientAddress' value={formInputData.clientAddress} onChange={handleChange} placeholder='14x Lekki' className='p-3 border-2 border-gray-700 rounded-md' />
                 </div>
 
                 {/* buttons */}
