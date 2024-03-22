4166816c027fbf806137b48dafebd4c65e2c323c
DCTP-BE <akpata.thelma@gmail.com>
2024-03-22 12:27:07 +0100

    v2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import moon from '../assets/icon-moon.svg';
 const Navbar = () => {
   return (
     <div>
- <div className='hidden md:block'>
+      <div className='hidden md:block'>
         <div className='fixed flex flex-col h-[100%] mx-auto rounded-e-3xl w-18 bg-slate-600'>
           <div>
             <img src={logo} />
@@ -22,28 +22,28 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-        </div>
+      </div>
 
 
 
 
       {/* Mobile View */}
 
-      <div className='flex justify-between md:hidden md:rounded-e-3xl md:bg-slate-600'>
+      <div className='flex justify-between md:hidden bg-slate-600'>
         <div className='md:mt-8 md:mb-auto md:text-center'>
           <img src={logo} alt="Logo" />
         </div>
-        <div className=' md:flex md:items-center md:gap-4 md:bottom-3'>
-          <div className='md:items'>
-            <img src={sun} alt="Sun Icon" />
+        <div className='flex items-center justify-center gap-4 mr-4'>
+          <div className='items-center'>
+            <img src={sun} alt="Sun Icon" className='w-8 h-8 rounded-full'/>
           </div>
-          {/* <div className='md:relative md:block md:w-4 md:h-[1.5px] md:bg-slate-400'></div> */}
-          <div>
-            <img src={avatar} alt="Avatar" className='md:w-2 md:h-12 md:rounded-full' />
+          <div className='relative block w-[0.9px] h-20 bg-slate-400'></div>
+          <div className='h-[80px] items-center flex'>
+            <img src={avatar} alt="Avatar" className='w-16 h-16 rounded-full' />
           </div>
-        </div> 
+        </div>
       </div>
-      </div>
+    </div>
   )
 }
 
