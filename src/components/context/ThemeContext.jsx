751c803cca53022d8d79e2f498ca2c0a924e6e6b
DCTP-BE <akpata.thelma@gmail.com>
2024-03-30 13:23:47 +0100

    v2

--- src/components/context/ThemeContext.jsx
+++ src/components/context/ThemeContext.jsx
@@ -4,3 +4,14 @@ export const ThemeContext = createContext({
     theme: "dark",
     toggleTheme: () => {},
 });
+
+export const Provider = ({ children }) => {
+    const [theme, setTheme] = useState("dark");
+
+    const toggleTheme = () =>{
+        setTheme(theme === "light" ? "dark" : "light");
+    }
+}
+
+<ThemeContext.Provider value={{ theme, toggleTheme }}></ThemeContext.Provider>
+import { ThemeContext, Provider } from "./ThemeContext";
