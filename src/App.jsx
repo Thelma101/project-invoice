4fa46b29497ffa9eccd5164ce3a7da00f52af4e9
DCTP-BE <akpata.thelma@gmail.com>
2024-04-02 14:01:36 +0100

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './index.css'
-import Navbar from './components/Navbar'
-import Invoices from './components/Invoices'
-import NewInvoice from './components/NewInvoice'
-import { ThemeContext } from './components/context/ThemeContext'
-import { Provider } from './components/context/ThemeContext'
-import Details from './components/Details'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { useState } from 'react';
+import './index.css';
+import Navbar from './components/Navbar';
+import Invoices from './components/Invoices';
+import NewInvoice from './components/NewInvoice';
+import { Provider } from './components/context/ThemeContext';
+import Layout from './components/Layout';
+import Details from './components/Details';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
 const router = createBrowserRouter([
@@ -22,10 +20,16 @@ const router = createBrowserRouter([
 
   return (
     <>
-      <Provider>
+<Provider>
+  <Layout>
+    <RouterProvider router = {router} />
+  </Layout>
+</Provider>
+    
+      {/* <Provider>
         <Navbar />
         <Invoices />
-      </Provider>
+      </Provider> */}
 
       {/* <NewInvoice /> */}
       {/* <h1 className='text-center text-teal-700 '>Hello, how you doing?</h1> */}
