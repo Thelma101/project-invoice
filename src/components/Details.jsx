8ba4bfddf33b2c2ff8799848e91cfccce278147d
DCTP-BE <akpata.thelma@gmail.com>
2024-04-02 19:21:21 +0100

    v2

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -5,7 +5,7 @@ import Invoices from './Invoices';
 
 const Details = () => {
     return (
-        <div className='mx-[300px]  mt-14'>
+        <div className='mx-32 md:mx-[300px]  mt-14'>
             <div className='mx-auto text-gray-200 '>
                 <Link to={`'/' ${<Invoices />}`} className="flex items-center justify-center gap-2">
                     <div className='justify-center text-center'>
@@ -20,14 +20,15 @@ const Details = () => {
                     <div className="flex items-center justify-between w-full mx-4">
                         <div className="flex items-center gap-3">
                             <h2>Status</h2>
-                            <div className="flex items-center justify-end gap-3 pt-3 pb-3 pl-5 pr-6 bg-green-900 rounded-lg ">
+                            <div className="flex items-center justify-end gap-3 px-3 py-2 bg-green-900 rounded-lg ">
+                                {/* <p className="text-sm font-light text-gray-300">{value.status}</p> */}
                                 <p className="text-sm font-light text-gray-300">Paid</p>
                             </div>
                         </div>
                         <div className="flex items-center gap-3">
-                            <button className="px-3 py-1 text-gray-500 rounded-full bg-custom-nav">Edit</button>
-                            <button className="px-3 py-1 text-white bg-red-500 rounded-full">Delete</button>
-                            <button className="hidden px-3 py-1 text-white bg-purple-400 rounded-full md:block">Mark as paid</button>
+                            <button className="px-3 py-1 text-sm text-gray-500 bg-gray-300 rounded-full">Edit</button>
+                            <button className="px-3 py-1 text-sm text-white bg-red-500 rounded-full">Delete</button>
+                            <button className="hidden px-3 py-1 text-sm text-white bg-purple-400 rounded-full md:block">Mark as paid</button>
                         </div>
                     </div>
                 </div>
@@ -37,32 +38,8 @@ const Details = () => {
     );
 }
 
-// return (
-//   <div className="md:mt-[3rem] gap-3 flex flex-col justify-center items-center w-full relative">
-//         <Link to="/" className="flex items-center justify-center gap-2">
-//             <img src="./icon-arrow-left.svg" alt="Back" />
-//             Back
-//         </Link>
-//         <div className="w-full md:h-[4.5rem] flex justify-center items-center bg-custom-base rounded-lg">
-//             <div className="flex items-center justify-between w-full mx-4">
-//                 <div className="flex items-center gap-3">
-//                     <h2>Status</h2>
-//                     <div className="flex items-center justify-end gap-3 pt-3 pb-3 pl-5 pr-6 rounded-lg bg-custom-paid_1">
-//                         <p className="h-[10px] w-[10px] bg-custom-paid_2 rounded-full"></p>
-//                         <p className="text-sm font-light text-custom-paid">Paid</p>
-//                     </div>
-//                 </div>
-//                 <div className="flex items-center gap-3">
-//                     <button className="px-3 py-1 text-gray-500 rounded-full bg-custom-nav">Edit</button>
-//                     <button className="px-3 py-1 text-white bg-red-500 rounded-full">Delete</button>
-//                     <button className="hidden px-3 py-1 text-white bg-purple-400 rounded-full md:block">Mark as paid</button>
-//                 </div>
-//             </div>
-//         </div>
+
 //         <div className="relative flex items-center justify-center w-full p-8 mb-8 rounded-lg bg-custom-base">
-//             {/* Invoice content goes here */}
-//         </div>
-//     </div>
-// );
+
 
 export default Details;
