ac9b737e9b0087d6d94946755394f1454e81524d
DCTP-BE <akpata.thelma@gmail.com>
2024-04-02 18:13:38 +0100

    v1

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import leftArrow from '../assets/icon-arrow-left.svg';
+import { Link } from 'react-router-dom';
+import Invoices from './Invoices';
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
+    return (
+        <div>
+            <div className='mx-auto text-gray-200'>
+                <Link to={`'/:id' ${<Invoices />}`} className="flex items-center justify-center gap-2">
+                    <div className='justify-center text-center'>
+                        <img src={leftArrow} alt='leftpointer' className='text-[#7c5dfa]] mx-auto cursor-pointer ' />
+                        Back
+        {/* </Link> */}
+                    </div>
+                    </Link>
+                    <div className=''>
+                        <p className='mx-auto cursor-pointer text-text-access'>Back</p>
+                    </div>
+                {/* </Link> */}
+            </div>
+        </div >
+    )
 }
 
-export default Details
+// return (
+//     <div className="md:mt-[3rem] gap-3 flex flex-col justify-center items-center w-full relative">
+//         <Link to="/" className="flex items-center justify-center gap-2">
+//             <img src="./icon-arrow-left.svg" alt="Back" />
+//             Back
+//         </Link>
+//         <div className="w-full md:h-[4.5rem] flex justify-center items-center bg-custom-base rounded-lg">
+//             <div className="flex items-center justify-between w-full mx-4">
+//                 <div className="flex items-center gap-3">
+//                     <h2>Status</h2>
+//                     <div className="flex items-center justify-end gap-3 pt-3 pb-3 pl-5 pr-6 rounded-lg bg-custom-paid_1">
+//                         <p className="h-[10px] w-[10px] bg-custom-paid_2 rounded-full"></p>
+//                         <p className="text-sm font-light text-custom-paid">Paid</p>
+//                     </div>
+//                 </div>
+//                 <div className="flex items-center gap-3">
+//                     <button className="px-3 py-1 text-gray-500 rounded-full bg-custom-nav">Edit</button>
+//                     <button className="px-3 py-1 text-white bg-red-500 rounded-full">Delete</button>
+//                     <button className="hidden px-3 py-1 text-white bg-purple-400 rounded-full md:block">Mark as paid</button>
+//                 </div>
+//             </div>
+//         </div>
+//         <div className="relative flex items-center justify-center w-full p-8 mb-8 rounded-lg bg-custom-base">
+//             {/* Invoice content goes here */}
+//         </div>
+//     </div>
+// );
+
+// export default Details;
