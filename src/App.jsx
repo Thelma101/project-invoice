5bd2c3ba9b50e429e0c2ff4956c4bbdeae37e494
DCTP-BE <akpata.thelma@gmail.com>
2024-04-02 13:43:50 +0100

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
 import './index.css'
 import Navbar from './components/Navbar'
 import Invoices from './components/Invoices'
 import NewInvoice from './components/NewInvoice'
 import { ThemeContext } from './components/context/ThemeContext'
 import { Provider } from './components/context/ThemeContext'
+import Details from './components/Details'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    path : '/', element : <Invoices />
+  },
+  {
+    path : '/:id', element : <Details />
+  }
+])
 
   return (
     <>
