ac2503512b2d163e8baba2190baa095a34f48aa0
DCTP-BE <akpata.thelma@gmail.com>
2024-04-11 19:45:09 +0100

    v3

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -81,28 +81,29 @@ const Details = () => {
                     </div>
 
                     <div className="grid bg-[#1e2139] w-full items-center pt-6 rounded-t-lg rounded-b-lg mt-20">
-                        {data.items.map((item, index) => (
-                            <div key={index} className="grid grid-cols-4 md:grid-cols-8 bg-[#1e2139] mx-auto w-full pb-7" >
-                                <div className="flex flex-col items-center justify-start w-full col-span-2 text-start">
-                                    <h3 className="text-gray-400 text-start">Item name</h3>
-                                    <p className="text-center">{item.name}</p>
-                                </div>
-                                <div className="hidden col-span-2 md:flex md:flex-col md:items-center md:justify-start">
-                                    <h3 className="text-gray-400">Qty</h3>
-                                    <p>{item.quantity}</p>
-                                </div>
-                                <div className="hidden col-span-2 md:flex md:flex-col md:items-center md:justify-start">
-                                    <h3 className="text-gray-400">Item price</h3>
-                                    <p>{item.price}</p>
-                                </div>
-                                <div className="flex flex-col items-center justify-start">
-                                    <h3 className="text-gray-400">Total</h3>
-                                    <p>£{item.total}</p>
-                                </div>
+                        <div className="grid grid-cols-4 md:grid-cols-8 bg-[#1e2139] mx-auto w-full pb-7" >
+                            <div className="flex flex-col items-center justify-start w-full col-span-2 text-start">
+                                {data.items.map((item, index) => (
+                                    <div>
+                                        <div key={index} className="flex flex-col items-center justify-start w-full col-span-2 text-start">
+                                            <h3 className="text-gray-400 text-start">Item name</h3>
+                                            <p className="text-center">{item.name}</p>
+                                        </div><div className="hidden col-span-2 md:flex md:flex-col md:items-center md:justify-start">
+                                            <h3 className="text-gray-400">Qty</h3>
+                                            <p>{item.quantity}</p>
+                                        </div><div className="hidden col-span-2 md:flex md:flex-col md:items-center md:justify-start">
+                                            <h3 className="text-gray-400">Item price</h3>
+                                            <p>{item.price}</p>
+                                        </div><div className="flex flex-col items-center justify-start">
+                                            <h3 className="text-gray-400">Total</h3>
+                                            <p>£{item.total}</p>
+                                        </div>
+                                    </div>
                             </div>
-                //    </div>
-                        ))};
+                        </div>
 
+                        ))};
+                    </div>
                     <div className="bg-[#070a1a] rounded-b-lg h-[6rem] w-full mt-8">
                         <div className="flex items-start justify-between p-10 rounded-lg rounded-t-none">
                             <h2 className="text-2xl font-semibold text-white">{data.amount}</h2>
@@ -112,8 +113,10 @@ const Details = () => {
                 </div>
 
             </div>
-            </div>
-            );
+        </div>
+        </div >
+    );
 };
-</div>
+        </div >
+</div >
 export default Details;
