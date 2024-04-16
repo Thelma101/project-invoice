468aebaefa82d26c4a5ac957c3398ebfcac94322
DCTP-BE <akpata.thelma@gmail.com>
2024-04-16 18:07:52 +0100

    v4

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -29,8 +29,8 @@ const Details = () => {
                     <div className="flex items-center justify-between w-full mx-4">
                         <div className="flex items-center gap-3">
                             <h2 className='text-main-text'>Status</h2>
-                            <div className={`flex items-center justify-end gap-3 px-3 py-2 rounded-lg ${data.status === 'paid' ? 'bg-text-paid' : data.status === 'pending' ? 'bg-text-pending' : 'bg-text-draft'}`}>
-                                <p className="text-sm font-light text-gray-300">{data.status}</p>
+                            <div className={`flex items-center justify-end gap-3 px-3 py-2 rounded-lg ${data.status === 'paid' ? 'bg-[#166534]' : data.status === 'pending' ? 'bg-text-pending' : 'bg-text-draft'}`}>
+                                <p className={`{"text-sm font-light text-gray-300"} `}>{data.status}</p>
                                 {/* <p className="text-sm font-light text-gray-300">Paid</p> */}
                             </div>
                         </div>
